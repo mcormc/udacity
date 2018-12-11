2737e256387d64b1ddfd67f4716d09bbfd009f83
Jason McCormick <jasonhenrymccormick@gmail.com>
2018-12-11 14:57:31 -0800

    Update concatenation.py
    added part 2 of concatenation.py

--- concatenation.py
+++ concatenation.py
@@ -2,3 +2,8 @@ def adverbly(s):
     return s + 'ly'
 
 print(adverbly("quick"))
+
+name = input("What's your name?")
+print("\nHi, " + name + "! It's very nice to meet you.")
+color = input("What's your favorite color?")
+print("\nAh, " + color + ", what a lovely color!")
