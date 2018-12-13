f10c24c61ab019c49fbcec261ac0f5ba973cfaf4
Jason McCormick <jasonhenrymccormick@gmail.com>
2018-12-12 16:09:12 -0800

    Create starts_with_three.py
    File from udacity IPND

---
+++ starts_with_three.py
@@ -0,0 +1,16 @@
+def starts_with_v1(long, short):
+    for position in range(len(short)):
+        if long[position] != short[position]:
+            return False
+    return True
+
+def starts_with_v2(long, short):
+    length = len(short)
+    beginning = long[0 : length]
+    if beginning == short:
+        return True
+    else:
+        return False
+
+def starts_with_v3(long, short):
+    return long[0:len(short)] == short
