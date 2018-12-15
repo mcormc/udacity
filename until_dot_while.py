10fab9bdf3a81a46c3cb353e6eed5fe4eb725a15
Jason McCormick <jasonhenrymccormick@gmail.com>
2018-12-15 14:29:12 -0800

    Create until_dot_while.py

---
+++ until_dot_while.py
@@ -0,0 +1,9 @@
+def until_dot(s):
+    index = 0
+    while index < len(s) and s[index] != '.':
+        # No dots yet, keep going.
+        index += 1
+    # We either found a dot or ran out of string.
+    return s[:index]
+
+until_dot("This is a sentence. This is another sentence.")
