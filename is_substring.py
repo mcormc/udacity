9dd3f4439e760e254411c11118821e537474564d
Jason McCormick <jasonhenrymccormick@gmail.com>
2018-12-15 15:14:11 -0800

    Create is_substring.py

---
+++ is_substring.py
@@ -0,0 +1,7 @@
+def is_substring(substring, string):
+    index = 0
+    while index < len(string):
+        if string[index : index + len(substring)] == substring:
+            return True
+        index += 1
+    return False
