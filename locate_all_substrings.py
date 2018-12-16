87af21085f4a6cdd27afd6dd5e1816af41ef85a0
Jason McCormick <jasonhenrymccormick@gmail.com>
2018-12-15 16:15:37 -0800

    Create locate_all_substrings.py

---
+++ locate_all_substrings.py
@@ -0,0 +1,12 @@
+def locate_all(string, sub):
+    matches = []
+    index = 0
+    while index < len(string):
+        if string[index : index + len(sub)] == sub:
+            matches.append(index)
+            index += len(sub)
+        else:
+            index += 1
+    return matches
+
+print(locate_all('yesyesyes', 'yes'))
