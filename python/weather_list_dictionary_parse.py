fb661a474b7feab18aa05a4b190d68d697694a8f
Jason McCormick <jasonhenrymccormick@gmail.com>
2018-12-19 18:46:41 -0800

    Update weather_list_dictionary_parse.py

--- python/weather_list_dictionary_parse.py
+++ python/weather_list_dictionary_parse.py
@@ -14,6 +14,15 @@ weather = [
 for e in weather:
     print(e['date'] + ' will be ' + e['state'] + ' with a temperature of ' + str(e['temp']))
 
+# for forecast in weather:
+#    print(f"The weather for {forecast['date']} will be {forecast['state']} with a temperature of {forecast['temp']} degrees.")
+
+# for forecast in weather:
+#    date = forecast['date']
+#    state = forecast['state']
+#    temp = forecast['temp']
+#    print(f"The weather for {date} will be {state} with a temperature of {temp} degrees.")
+
 # for e in weather[0].values():
 #    print(e)
 
