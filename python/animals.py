a4c23fa2775040081cd9454db3b53b299b920961
Jason McCormick <jasonhenrymccormick@gmail.com>
2018-12-22 17:20:54 -0800

    Update animals.py

--- python/animals.py
+++ python/animals.py
@@ -1,6 +1,12 @@
 class Dog:
     def speak(self):
-        print("Woof!")
+        print("Bowow!")
+
+    def eat(self, string):
+        if string == "biscuit":
+            print("Yummy!")
+        else:
+            print("That's not food!")
 
 class Cat:
     def speak(self):
