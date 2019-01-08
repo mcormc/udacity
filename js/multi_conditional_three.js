73ea8115728d95a787328f98ec98a232562f0597
Jason McCormick <jasonhenrymccormick@gmail.com>
2019-01-08 15:23:15 -0800

    Create multi_conditional_three.js

---
+++ js/multi_conditional_three.js
@@ -0,0 +1,13 @@
+
+// if else purchase:
+
+var money = 100.50;
+var price = 100.50;
+
+if (money > price) {
+  console.log("You paid extra, here's your change.");
+} else if (money === price) {
+  console.log("You paid the exact amount, have a nice day!");
+} else {
+  console.log("That's not enough, you still owe me money.");
+}
