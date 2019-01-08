a1049e82a8060d685ba22fc69c9665a35bc9bb69
Jason McCormick <jasonhenrymccormick@gmail.com>
2019-01-08 15:21:59 -0800

    Delete if_else_weather.js

--- js/if_else_weather.js
+++
@@ -1,9 +0,0 @@
-var weather = "sunny";
-
-if (weather === "snow") {
-  console.log("Bring a coat.");
-} else if (weather === "rain") {
-  console.log("Bring a rain jacket.");
-} else {
-  console.log("Wear what you have on.");
-}
