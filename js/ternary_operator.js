798890541f4d9d4ebb11986b3d526c6e36f4b6ed
Jason McCormick <jasonhenrymccormick@gmail.com>
2019-01-24 14:43:14 -0800

    Update ternary_operator.js

--- js/ternary_operator.js
+++ js/ternary_operator.js
@@ -1,10 +1,18 @@
+// var isGoing = true;
+// var color;
+
+// if (isGoing) {
+//   color = "green";
+// } else {
+//   color = "red";
+// }
+
+// console.log(color);
+
+// SAME AS...
+
+// conditional ? (if condition is true) : (if condition is false)
+
 var isGoing = true;
-var color;
-
-if (isGoing) {
-  color = "green";
-} else {
-  color = "red";
-}
-
+var color = isGoing ? "green" : "red";
 console.log(color);
