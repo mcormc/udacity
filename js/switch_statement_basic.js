fc862e06f4b89d1def667186318eb673f362a75e
Jason McCormick <jasonhenrymccormick@gmail.com>
2019-01-24 15:46:19 -0800

    Create switch_statement_basic.js

---
+++ js/switch_statement_basic.js
@@ -0,0 +1,14 @@
+switch (option) {
+  case 1:
+    console.log("You selected option 1.");
+  case 2:
+    console.log("You selected option 2.");
+  case 3:
+    console.log("You selected option 3.");
+  case 4:
+    console.log("You selected option 4.");
+  case 5:
+    console.log("You selected option 5.");
+  case 6:
+    console.log("You selected option 6.");
+}
