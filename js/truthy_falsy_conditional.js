e81a5146f02c2f551f7ae1e6c26c3221eef894e3
Jason McCormick <jasonhenrymccormick@gmail.com>
2019-01-24 14:18:23 -0800

    Create truthy_falsy_conditional.js

---
+++ js/truthy_falsy_conditional.js
@@ -0,0 +1,4 @@
+var adult = true;
+var preorder = true;
+
+console.log("It costs $" + (adult ? "40.00" : "20.00") + " to attend the concert. Pick up your tickets at the " + (preorder ? "will call" : "gate") + ".");
