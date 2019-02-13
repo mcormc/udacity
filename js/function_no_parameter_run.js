047f24e9abecb691aec5afdd0b23741464a7612e
Jason McCormick <jasonhenrymccormick@gmail.com>
2019-02-13 14:48:07 -0800

    Create function_no_parameter_run.js

---
+++ js/function_no_parameter_run.js
@@ -0,0 +1,8 @@
+// declares the sayHello function
+function sayHello() {
+  var message = "Hello!"
+  return message; // returns value instead of printing it
+}
+
+// function returns "Hello!" and console.log prints the return value
+console.log(sayHello());
