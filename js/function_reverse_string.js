20ed6ff34a2dcfde4916fbd0165cd0a40aa216c0
Jason McCormick <jasonhenrymccormick@gmail.com>
2019-02-13 14:44:28 -0800

    Create function_reverse_string.js

---
+++ js/function_reverse_string.js
@@ -0,0 +1,9 @@
+function reverseString(reverseMe) {
+    var reversed = "";
+    for (var i = reverseMe.length - 1; i >= 0; i--) {
+        reversed += reverseMe[i];
+    }
+    return reversed;
+}
+
+console.log(reverseString("Jason"));
