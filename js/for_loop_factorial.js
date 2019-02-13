f8da68f2c446cf284923621fcec1c0902c7ab84e
Jason McCormick <jasonhenrymccormick@gmail.com>
2019-02-13 14:31:52 -0800

    Update for_loop_factorial.js

--- js/for_loop_factorial.js
+++ js/for_loop_factorial.js
@@ -9,3 +9,13 @@ for (var x = 1; x < 12; x++) {
     solution *= x;
 }
 console.log(solution);
+
+// or...
+
+let solution = 1;
+
+for (let i = 1; i <= 12; i++) {
+  solution *= i;
+}
+
+console.log(solution);
