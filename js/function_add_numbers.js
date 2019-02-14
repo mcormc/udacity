6161728a68fb0e4d0672b5ea0d0151a8c9a2a707
Jason McCormick <jasonhenrymccormick@gmail.com>
2019-02-14 12:26:36 -0800

    Create function_add_numbers.js

---
+++ js/function_add_numbers.js
@@ -0,0 +1,9 @@
+// x and y are parameters in this function declaration
+function add(x, y) {
+  // function body
+  var sum = x + y;
+  return sum; // return statement
+}
+
+// 1 and 2 are passed into the function as arguments
+var sum = add(1, 2);
