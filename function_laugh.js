35e2edcf56332bb610ce722cfaacf3c5ee761162
Jason McCormick <jasonhenrymccormick@gmail.com>
2019-02-14 12:28:58 -0800

    Create function_laugh.js

---
+++ function_laugh.js
@@ -0,0 +1,12 @@
+/*
+ * Programming Quiz: Laugh it Off 1 (5-1)
+ */
+
+// your code goes here
+function laugh() {
+    var laughter = "hahahahahahahahahaha!";
+    return laughter;
+}
+
+console.log(laugh());
+
