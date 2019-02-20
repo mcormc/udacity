040a7578037bc2c3a3de48039414b7dbecf4aac8
Jason McCormick <jasonhenrymccormick@gmail.com>
2019-02-20 12:46:39 -0800

    Create array_examples.js

---
+++ js/array_examples.js
@@ -0,0 +1,16 @@
+// creates a `donuts` array with three strings
+var donuts = ["glazed", "powdered", "jelly"];
+
+// creates a `mixedData` array with mixed data types
+var mixedData = ["abcd", 1, true, undefined, null, "all the things"];
+
+// creates a `arraysInArrays` array with three arrays
+var arraysInArrays = [[1, 2, 3], ["Julia", "James"], [true, false, true, false]];
+
+// Nested arrays can be particularly hard to read, so it's common to 
+// write them on one line, using a newline after each comma:
+var arraysInArrays = [
+  [1, 2, 3], 
+  ["Julia", "James"], 
+  [true, false, true, false]
+];
