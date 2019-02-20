beef03cd88c672a69d4e19c44c38c9e5714f8bc8
Jason McCormick <jasonhenrymccormick@gmail.com>
2019-02-20 12:49:36 -0800

    Create array_indexing_basic.js

---
+++ js/array_indexing_basic.js
@@ -0,0 +1,3 @@
+var donuts = ["glazed", "powdered", "sprinkled"];
+console.log(donuts[0]); // "glazed" is the first element in the `donuts` array
+console.log(donuts[3]); // the fourth element in `donuts` array does not exist! prints `undefined`
