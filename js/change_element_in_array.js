d97b896f77bae8c44d7b970c276a91125803de32
Jason McCormick <jasonhenrymccormick@gmail.com>
2019-02-20 13:17:35 -0800

    Create change_element_in_array.js

---
+++ js/change_element_in_array.js
@@ -0,0 +1,8 @@
+var donuts = ["glazed", "chocolate frosted", "boston cream", "powdered", "sprinkled", "maple", "coconut", "jelly"];
+
+
+donuts[2] = "cinnamon twist";
+donuts[4] = "salted caramel";
+donuts[5] = "shortcake eclair";
+
+// now, it's var donuts = ["glazed", "chocolate frosted", "cinnamon twist", "powdered", "salted caramel", "shortcake eclair", "coconut", "jelly"]
