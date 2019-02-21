bc991503b10b3c12e9b1e3aa20d22f418e935ee8
Jason McCormick <jasonhenrymccormick@gmail.com>
2019-02-21 13:36:12 -0800

    Update array_push_simple.js

--- js/array_push_simple.js
+++ js/array_push_simple.js
@@ -1,2 +1,6 @@
 var donuts = ["glazed", "chocolate frosted", "Boston creme", "glazed cruller", "cinnamon sugar", "sprinkled"];
 donuts.push("powdered"); // pushes "powdered" onto the end of the `donuts` array
+
+// Returns: 7
+// donuts array: ["glazed", "chocolate frosted", "Boston creme", "glazed cruller", "cinnamon sugar", "sprinkled", "powdered"]
+
