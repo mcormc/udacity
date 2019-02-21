d35d8f8cf43d665014db9d375e46bad515ec3e59
Jason McCormick <jasonhenrymccormick@gmail.com>
2019-02-21 13:38:14 -0800

    Create array_pop_simple.js

---
+++ js/array_pop_simple.js
@@ -0,0 +1,8 @@
+var donuts = ["glazed", "chocolate frosted", "Boston creme", "glazed cruller", "cinnamon sugar", "sprinkled", "powdered"];
+
+donuts.pop(); // pops "powdered" off the end of the `donuts` array
+donuts.pop(); // pops "sprinkled" off the end of the `donuts` array
+donuts.pop(); // pops "cinnamon sugar" off the end of the `donuts` array
+
+// Returns: "cinnamon sugar"
+// donuts array: ["glazed", "chocolate frosted", "Boston creme", "glazed cruller"]
