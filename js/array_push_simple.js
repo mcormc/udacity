009049a12a4a78b94070644d28d9986aa80157ad
Jason McCormick <jasonhenrymccormick@gmail.com>
2019-02-21 13:35:27 -0800

    Create array_push_simple.js

---
+++ js/array_push_simple.js
@@ -0,0 +1,2 @@
+var donuts = ["glazed", "chocolate frosted", "Boston creme", "glazed cruller", "cinnamon sugar", "sprinkled"];
+donuts.push("powdered"); // pushes "powdered" onto the end of the `donuts` array
