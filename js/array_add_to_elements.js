63cc36806ee50db043943952992537bbdd23436b
Jason McCormick <jasonhenrymccormick@gmail.com>
2019-02-21 14:29:23 -0800

    Create array_add_to_elements.js

---
+++ js/array_add_to_elements.js
@@ -0,0 +1,7 @@
+var donuts = ["jelly donut", "chocolate donut", "glazed donut"];
+
+donuts[0] += " hole";
+donuts[1] += " hole";
+donuts[2] += " hole";
+
+// donuts array: ["jelly donut hole", "chocolate donut hole", "glazed donut hole"]
