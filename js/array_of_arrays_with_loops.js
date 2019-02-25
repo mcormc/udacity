e86e55eb454998fdc7938c533babadaa3a742a7f
Jason McCormick <jasonhenrymccormick@gmail.com>
2019-02-25 13:03:49 -0800

    Create array_of_arrays_with_loops.js

---
+++ js/array_of_arrays_with_loops.js
@@ -0,0 +1,23 @@
+var donutBox = [
+  ["glazed", "chocolate glazed", "cinnamon"],
+  ["powdered", "sprinkled", "glazed cruller"],
+  ["chocolate cruller", "Boston creme", "creme de leche"]
+];
+
+for (var row = 0; row < donutBox.length; row++) {
+  // here, donutBox[row].length refers to the length of the donut array currently being looped over
+  for (var column = 0; column < donutBox[row].length; column++) {
+    console.log(donutBox[row][column]);
+  }
+}
+
+ // Prints:
+ // "glazed"
+ // "chocolate glazed"
+ // "cinnamon"
+ // "powdered"
+ // "sprinkled"
+ // "glazed cruller"
+ // "chocolate cruller"
+ // "Boston creme"
+ // "creme de leche"
