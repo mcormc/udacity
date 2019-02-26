b682952676a2fcddf0c187bb214cdfef8724529d
Jason McCormick <jasonhenrymccormick@gmail.com>
2019-02-26 13:58:00 -0800

    Create typeof_operator.js

---
+++ js/typeof_operator.js
@@ -0,0 +1,4 @@
+typeof "hello" // returns "string"
+typeof true // returns "boolean"
+typeof [1, 2, 3] // returns "object" (Arrays are a type of object)
+typeof function hello() { } // returns "function"
