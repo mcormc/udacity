05199347b18289d43dfd332b9b2dafb6ef664b33
Jason McCormick <jasonhenrymccormick@gmail.com>
2019-02-28 15:16:44 -0800

    Create query_selector_method.js

---
+++ js/query_selector_method.js
@@ -0,0 +1,10 @@
+// https://developer.mozilla.org/en-US/docs/Web/API/Document/querySelector
+
+// find and return the element with an ID of "header"
+document.querySelector('#header');
+
+// find and return the first element with the class "header"
+document.querySelector('.header');
+
+// find and return the first <header> element
+document.querySelector('header');
