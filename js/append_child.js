b9e5c16fad6ced73ff24c38ca360fa4f4268d3b1
Jason McCormick <jasonhenrymccormick@gmail.com>
2019-03-04 13:06:09 -0800

    Create append_child.js

---
+++ js/append_child.js
@@ -0,0 +1,8 @@
+// create a brand new <span> element
+const newSpan = document.createElement('span');
+
+// select the first (main) heading of the page
+const mainHeading = document.querySelector('h1');
+
+// add the the <span> element as the last child element of the main heading
+mainHeading.appendChild(newSpan);
