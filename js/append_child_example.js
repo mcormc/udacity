d4cb2575161be151b11835ac7584d57da6a8c8e6
Jason McCormick <jasonhenrymccormick@gmail.com>
2019-03-04 13:11:42 -0800

    Create append_child_example.js

---
+++ js/append_child_example.js
@@ -0,0 +1,9 @@
+const mainHeading = document.querySelector('#main-heading');
+const otherHeading = document.querySelector('#other-heading');
+const excitedText = document.createElement('span');
+
+excitedText.textContent = '!!!';
+mainHeading.appendChild(excitedText);
+otherHeading.appendChild(excitedText);
+
+// only otherHeading has three exclamation marks
