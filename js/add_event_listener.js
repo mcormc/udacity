34fe8ddc6a16d1c0889112963d03541892f9410f
Jason McCormick <jasonhenrymccormick@gmail.com>
2019-03-05 14:44:35 -0800

    Create add_event_listener.js

---
+++ js/add_event_listener.js
@@ -0,0 +1,7 @@
+const mainHeading = document.querySelector('h1');
+
+mainHeading.addEventListener('click', function () {
+  console.log('The heading was clicked!');
+});
+
+// https://developer.mozilla.org/en-US/docs/Web/API/EventTarget/addEventListener
