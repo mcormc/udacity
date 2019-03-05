864064c3a308be9168b460551e29c41404fd76fa
Jason McCormick <jasonhenrymccormick@gmail.com>
2019-03-05 14:17:21 -0800

    Create class_list.js

---
+++ js/class_list.js
@@ -0,0 +1,9 @@
+// <h1 id="main-heading" class="ank-student jpk-modal">Learn Web Development at Udacity</h1>
+
+const mainHeading = document.querySelector('#main-heading');
+
+// store the list of classes in a variable
+const listOfClasses = mainHeading.classList;
+
+// logs out ["ank-student", "jpk-modal"]
+console.log(listOfClasses);
