a22c7c3d9d6d704685993c6e49de5d90a59cc2b1
Jason McCormick <jasonhenrymccormick@gmail.com>
2019-03-05 15:03:01 -0800

    Create remove_event_listener.js

---
+++ js/remove_event_listener.js
@@ -0,0 +1,9 @@
+function myEventListeningFunction() {
+    console.log('howdy');
+}
+
+// adds a listener for clicks, to run the `myEventListeningFunction` function
+document.addEventListener('click', myEventListeningFunction);
+
+// immediately removes the click listener that should run the `myEventListeningFunction` function
+document.removeEventListener('click', myEventListeningFunction);
