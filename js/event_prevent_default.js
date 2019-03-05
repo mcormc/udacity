b34a308b87701a1befa5e65b9c1a3896367563bd
Jason McCormick <jasonhenrymccormick@gmail.com>
2019-03-05 15:19:35 -0800

    Create event_prevent_default.js

---
+++ js/event_prevent_default.js
@@ -0,0 +1,7 @@
+const links = document.querySelectorAll('a');
+const thirdLink = links[2];
+
+thirdLink.addEventListener('click', function (event) {
+    event.preventDefault();
+    console.log("Look, ma! We didn't navigate to a new page!");
+});
