323e3a3d01cfee43ce1d38d4bec7742844c0ca52
Jason McCormick <jasonhenrymccormick@gmail.com>
2019-03-05 14:01:20 -0800

    Create set_elements_attributes.js

---
+++ js/set_elements_attributes.js
@@ -0,0 +1,13 @@
+const mainHeading = document.querySelector('h1');
+
+mainHeading.setAttribute('style', 'color: blue; background-color: orange; font-size: 3.5em;');
+
+// also...
+
+const mainHeading = document.querySelector('h1');
+
+// add an ID to the heading's sibling element
+mainHeading.nextElementSibling.setAttribute('id', 'heading-sibling');
+
+// use the newly added ID to access that element
+document.querySelector('#heading-sibling').style.backgroundColor = 'red';
